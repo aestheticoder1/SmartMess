8c4599b38361966b52be1fe9e4cda0b3a40265a6
Nihal Rawat <nihalroyals.03@gmail.com>
2025-06-21 14:20:55 +0530

    Add vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Analytics />
     </Provider>
   </StrictMode>,
 )
