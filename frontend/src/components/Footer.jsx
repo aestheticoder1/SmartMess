edd151ea4459e06d15c0bca3d4281eda4d7ca7a4
Nihal Rawat <nihalroyals.03@gmail.com>
2025-06-28 17:05:08 +0530

    Re-update Footer Component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,13 @@
 const Footer = () => {
-    return (
-      <footer className="bg-black text-white py-6">
-        <div className="max-w-6xl mx-auto px-4 text-center">
-          <p className="text-sm md:text-base">
-            © {new Date().getFullYear()} SmartMess. Made with ☕ and ❤️.
-          </p>
-        </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <footer className="bg-black text-white py-6">
+      <div className="max-w-6xl mx-auto px-4 text-center">
+        <p className="text-sm md:text-base">
+          © {new Date().getFullYear()} SmartMess. Made with ☕ and ❤️ by Nihal Rawat.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
